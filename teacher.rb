0b47a806af5cca669cbecea9100f98534dd3ff0a
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-18 13:03:07 -0300

    Add class Teacher and its requirements

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require './person'
+
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(specialization, _age, _name = 'Unknown', _parent_permission: true)
+    super()
+    @specialization = specialization
+  end
+
+  def can_use_servicies?
+    true
+  end
+end
