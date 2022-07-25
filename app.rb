6d93a37f21803af869c82cfacd19382fa64773f1
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-25 14:09:13 -0300

    fix linters

--- app.rb
+++ app.rb
@@ -9,14 +9,14 @@ class App
   attr_accessor :book_list, :people_list
   attr_reader :user_options
 
-    def initialize
+  def initialize
     @user_options = ['List all books', 'List all people', 'Create a person', 'Create a book', 'Create a rental',
                      'List all rentals for a given person id', 'Exit']
     @state = { book_list: [], people_list: [], rental_list: [], keep_going: true }
   end
 
   def run
-     while @state[:keep_going]
+    while @state[:keep_going]
       p 'Please choose an option by entering a number:'
       @user_options.each_with_index do |option, index|
         p "#{index + 1} - #{option}"
