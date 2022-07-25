a5a8dc71a5a1d0dd5c7f74e82154f912689c3393
Trictonicmp <code_shadow@hotmail.com>
2022-07-25 12:53:49 -0500

    Change: Refactor ListPeople and ListBooks classes to display with index

--- create_rental.rb
+++ create_rental.rb
@@ -1,20 +1,21 @@
+require_relative 'list_books'
+require_relative 'list_people'
+
 class CreateRental
   def initialize(state)
     @book_list = state[:book_list]
     @people_list = state[:people_list]
     @rental_list = state[:rental_list]
-  end
+    @list_books = ListBooks.new(state)
+    @list_people = ListPeople.new(state)
+  end 
 
   def ask_user_input
     p 'Select a book from the following list by number'
-    @book_list.each_with_index do |book, index|
-      p "#{index}) Title: '#{book.title}', Author: #{book.author}"
-    end
+    @list_books.list_books_with_index
     book = @book_list[gets.chomp.to_i]
     p 'Select a person from the following list by number (not id)'
-    @people_list.each_with_index do |person, index|
-      p "#{index}) [#{person.class.name}] Name: '#{person.name}', ID: #{person.id}, Age: #{person.age}"
-    end
+    @list_people.list_people_with_index
     person = @people_list[gets.chomp.to_i]
     print 'Date: '
     date = gets.chomp
