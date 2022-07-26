a5bed53c53c30869b996c42ab40ce0eeadd167f0
Trictonicmp <code_shadow@hotmail.com>
2022-07-26 14:39:23 -0500

    Add test for person class can_use_services? with different ages and parent_permission

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -18,26 +18,48 @@ describe Person do
   describe 'Getters and Setters' do
     person = Person.new(20, 'John', parent_permission: false)
 
+    person_name = person.name
     it 'returns name of "John"' do
-      expect(person.name).to eql('John')
+      expect(person_name).to eql('John')
     end
 
+    person_age = person.age
     it 'returns age of 20' do
-      expect(person.age).to eql(20)
+      expect(person_age).to eql(20)
     end
 
-    it 'returns name "John"' do
-      expect(person.name).to eql('John')
-    end
+
+    person.name = 'Raul'
+    person_name2 = person.name
 
     it "sets person's name to \"Raul\"" do
-      person.name = 'Raul'
-      expect(person.name).to eql('Raul')
+      expect(person_name2).to eql('Raul')
     end
 
+    person.age = 16
+    person_age2 = person.age
     it "sets person's age to 16" do
-      person.age = 16
-      expect(person.age).to eql(16)
+      expect(person_age2).to eql(16)
+    end
+  end
+
+  describe 'Can Use Services' do
+    person = Person.new(20, 'John', parent_permission: false)
+    can_use_services = person.can_use_services?
+    it 'returns true due to age 20 and parent_permission false' do
+      expect(can_use_services).to eql(true)
+    end
+
+    person2 = Person.new(16, 'John', parent_permission: false)
+    can_use_services2 = person2.can_use_services?
+    it 'returns false due to age 16 and parent permission true' do
+      expect(can_use_services2).to eql(false)
+    end
+
+    person3 = Person.new(16, 'John', parent_permission: true)
+    can_use_services3 = person3.can_use_services?
+    it 'returns true due to age 16 and parent_permission true' do
+      expect(can_use_services3).to eql(true)
     end
   end
 end
