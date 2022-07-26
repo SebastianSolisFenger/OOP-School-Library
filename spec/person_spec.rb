7bbb797a229c2a2f7a79bba93b2bc6be5adbf9d0
Trictonicmp <code_shadow@hotmail.com>
2022-07-26 14:33:59 -0500

    Add test for person class setters and getters

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -14,4 +14,30 @@ describe Person do
       expect(can_use_setvices).to eql(true)
     end
   end
+
+  describe 'Getters and Setters' do
+    person = Person.new(20, 'John', parent_permission: false)
+
+    it 'returns name of "John"' do
+      expect(person.name).to eql('John')
+    end
+
+    it 'returns age of 20' do
+      expect(person.age).to eql(20)
+    end
+
+    it 'returns name "John"' do
+      expect(person.name).to eql('John')
+    end
+
+    it "sets person's name to \"Raul\"" do
+      person.name = 'Raul'
+      expect(person.name).to eql('Raul')
+    end
+
+    it "sets person's age to 16" do
+      person.age = 16
+      expect(person.age).to eql(16)
+    end
+  end
 end
