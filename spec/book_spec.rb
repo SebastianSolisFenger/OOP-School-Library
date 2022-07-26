9b77a885de6ad0c1e94b69bdbb81edc5d394ca9e
Trictonicmp <code_shadow@hotmail.com>
2022-07-26 15:35:44 -0500

    refactor book tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,14 +4,17 @@ describe Book do
   describe 'Create a new book' do
     title = 'Clean Code'
     author = 'Robert C. Martin'
-    newBook = Book.new(title, author)
+    book = Book.new(title, author)
+
+    book_title = book.title
+    book_author = book.author
 
     it 'is a book with title "Clean Code"' do
-      expect(newBook.title).to eql('Clean Code')
+      expect(book_title).to eql('Clean Code')
     end
 
     it 'is a book with author "Robert C. Martin"' do
-      expect(newBook.author).to eql('Robert C. Martin')
+      expect(book_author).to eql('Robert C. Martin')
     end
   end
 end
